483b3643f7575fcb90c1b271e8cdab7a4a056c6b
Havish Soowamber <soowamhc@gmail.com>
2023-02-22 13:45:02 +0400

    Added all necessary validations and associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, acceptance: { accept: %w[chinese italian japanese french belgian] }
+
+  has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  validates :rating, presence: true, inclusion: { in: 0..5, message: 'should be between 0 to 5' }
+  validates :rating, numericality: { only_integer: true }
+  validates :content, presence: true
+
+  belongs_to :restaurant
 end
